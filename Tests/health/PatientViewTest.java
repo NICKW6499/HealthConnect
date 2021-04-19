bf756fd68b0b852873907b331c1ae66559fb3b4f
Nicholis Wright <nickoliz414@gmail.com>
2021-04-18 20:58:48 -0400

    unit testing

--- Tests/health/PatientViewTest.java
+++ Tests/health/PatientViewTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import javax.swing.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PatientViewTest {
